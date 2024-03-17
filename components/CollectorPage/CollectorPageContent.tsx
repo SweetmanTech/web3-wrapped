a241531c36cba16e657c831c5911c6ea3e10e4d0
Sweets Sweetman <sweetmantech@gmail.com>
2024-03-17 19:50:57 -0300

    I remove dev logging.

--- components/CollectorPage/CollectorPageContent.tsx
+++ components/CollectorPage/CollectorPageContent.tsx
@@ -5,13 +5,11 @@ import Results from './Results';
 
 const CollectorPageContent = () => {
   const { collectorAddress, snapshot, ensName, profilePic } = useCollectorProvider();
-  console.log('SWEETS SNAPSHOT', snapshot);
 
   const totalNumberOfEditions = (snapshot as any).reduce(
     (total: number, item: any) => total + item.numberOfEditions,
     0,
   );
-  console.log('sweets Total Number of Editions:', totalNumberOfEditions);
 
   return (
     <div className="container flex flex-col items-center justify-center gap-4 px-4 text-center sm:gap-8 md:px-6">
